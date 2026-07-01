a13dbf6cec97c887b93850432db04ddfb8a9c0fd
rchmdndy <rachmadandyms@gmail.com>
2026-07-01 14:05:46 +0700

    feat : gtambah bperubahab

--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -3,6 +3,8 @@ package com.leszko.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+# perubahan
+
 @SpringBootApplication
 public class CalculatorApplication {
     public static void main(String[] args) {
